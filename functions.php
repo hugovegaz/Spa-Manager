bac903f2993344343d97249cc89e2a9542de7db6
hugovegaz <hugo.vega.z@gmail.com>
2024-12-14 01:17:16 -0500

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,1 +1,17 @@
+<?php
+// Registrar estilos y scripts
+function spa_manager_enqueue_scripts() {
+    wp_enqueue_style('style', get_stylesheet_uri());
+}
+add_action('wp_enqueue_scripts', 'spa_manager_enqueue_scripts');
 
+// Configurar soporte del tema
+function spa_manager_setup() {
+    add_theme_support('title-tag');
+    add_theme_support('post-thumbnails');
+    register_nav_menus(array(
+        'primary' => __('Menú Principal', 'spa-manager'),
+    ));
+}
+add_action('after_setup_theme', 'spa_manager_setup');
+?>
